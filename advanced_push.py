4dabd1c00d2c9d1dd09db592186d82eef5a09b95
benrap <Ben.rapoport@gmail.com>
2017-12-16 13:44:19 +0200

    Update advanced_push.py

--- advanced_push.py
+++ advanced_push.py
@@ -1,6 +1,7 @@
 def advanced_push(my_ships, team_capsule_holder, enemies, enemy_capsule_holder, enemy_mothership):
     push_together_threshold = 100
     ships_not_assigned = my_ships
+    enemy_ships_not_pushed = enemies
     move_threshold = 3
     closest_to_capsule_holder = sorted(ships_not_assigned, key=lambda d: d.distance(enemy_capsule_holder))
     if closest_to_capsule_holder[1].can_push(enemy_capsule_holder):
@@ -14,6 +15,7 @@ def advanced_push(my_ships, team_capsule_holder, enemies, enemy_capsule_holder, 
         ships_not_assigned.remove(closest_to_capsule_holder[0])
         ships_not_assigned.remove(closest_to_capsule_holder[1])
         ships_not_assigned.remove(team_capsule_holder)
+        enemy_ships_not_pushed.remove(enemy_capsule_holder)
     else:
         ships_not_assigned.remove(team_capsule_holder)
         closest_to_capsule_holder.remove(team_capsule_holder)
@@ -27,8 +29,13 @@ def advanced_push(my_ships, team_capsule_holder, enemies, enemy_capsule_holder, 
             ships_not_assigned.remove(closest_to_capsule_holder[1])
             ships_not_assigned.remove(closest_to_capsule_holder[0])
 
-    # TODO: make all other ships left push other enemy ships
-
+    # TODO: make all other ships left push other enemy ships in a smart manner
+    for ship in ships_not_assigned:
+        for enemy in enemy_ships_not_pushed:
+            if ship.can_push(enemy):
+                backwards_direction = enemy.get_location().add(multiply_location_vector(enemy.get_location().subtract(expected_location(enemy)), 100 ))
+                ship.push(enemy, backwards_direction)
+                enemy_ships_not_pushed.remove(enemy)
 
 def can_be_at_location(ship, location, num_of_turns):
     if ship.get_location().distance(location) < num_of_turns * MOVE_DISTANCE:
@@ -65,3 +72,6 @@ def can_be_pushed_to_wall(enemy, num_of_ships_pushing=1):
     if expected_location(enemy).distance(closest_wall(expected_location(enemy))) < num_of_ships_pushing * PUSH_DISTANCE:
         return True, closest_wall(expected_location(enemy))
     return False, None
+
+def multiply_location_vector(vector, multiplier):
+    return Location(vector.row*multiplier, vector.col*multiplier)
